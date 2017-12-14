b1b4fc4eddd01fc4b1986a0d9d8663e93e7d9ab9
jobidike <jobidike@gmail.com>
2017-12-13 21:19:36 -0500

    second

---
+++ src/main/java/com/formsubmission/FormSubmissionApplication.java
@@ -0,0 +1,12 @@
+package com.formsubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormSubmissionApplication.class, args);
+	}
+}
